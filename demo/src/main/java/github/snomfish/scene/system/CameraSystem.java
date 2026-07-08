02696428c49f680c5642b63f7ed1224a215cb493
SnomFish <arthurbaverstock21@gmail.com>
2026-07-08 21:33:52 +0100

    added camera system

--- demo/src/main/java/github/snomfish/scene/system/CameraSystem.java
+++ demo/src/main/java/github/snomfish/scene/system/CameraSystem.java
@@ -1,5 +1,7 @@
 package github.snomfish.scene.system;
 
+import org.joml.Vector3f;
+
 import github.snomfish.scene.Scene;
 import github.snomfish.scene.components.CameraCmp;
 import github.snomfish.scene.components.TransformCmp;
@@ -11,7 +13,35 @@ public class CameraSystem {
         
         for (Integer id : scene.getEntitiesWith(CameraCmp.class)) {
 
-            TransformCmp transform = scene.getComponent(id, TransformCmp.class);
+            CameraCmp c = scene.getComponent(id, CameraCmp.class);
+            TransformCmp t = scene.getComponent(id, TransformCmp.class);
+
+            if (t == null) continue;
+            //if (c.getUpdateFlag()) continue;
+            
+            Vector3f pos = t.getPosition();
+            Vector3f rot = t.getRotation();
+
+            // view
+            c.getView().identity()
+                .rotateXYZ(
+                    (float) Math.toRadians(rot.x),    
+                    (float) Math.toRadians(rot.y),
+                    (float) Math.toRadians(rot.z)
+                )
+                .translate(-pos.x, -pos.y, -pos.z);
+
+
+            // PROJECTION
+            c.getProjection().identity()
+                .perspective(
+                    (float)Math.toRadians(c.getFov()),
+                    aspect,
+                    c.getNear(),
+                    c.getFar()
+                );
+
+            c.setUpdateFlag(false);
         }
     }
 }
